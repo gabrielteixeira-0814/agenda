7ce76396cfb7785cf11a001cfb6ac667cac3bf24
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2023-07-17 21:15:21 -0300

    Inserindo template de formulario

--- core/views.py
+++ core/views.py
@@ -19,9 +19,6 @@ def logout_user(request):
     logout(request)
     return redirect('/')
 
-
-
-
 @csrf_exempt
 def submit_login(request):
     if request.POST:
@@ -46,3 +43,23 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario) # filtro por autenticação
     dados = {'eventos' : evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+@csrf_exempt
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    
+    return redirect('/')
